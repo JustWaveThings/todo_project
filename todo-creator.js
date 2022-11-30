6cf98345270adbcc4de1f4cee4dd8d45ee9b777b
JustWaveThings <justwavethings@proton.me>
2022-11-30 18:04:40 -0500

    Start constructor

---
+++ todo-creator.js
@@ -0,0 +1,15 @@
+new (class todoCreate {
+	constructor() {
+		this.uniqueKey = uniqueKey;
+		this.title = title;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.dueStatus = dueStatus;
+		this.soonestTodoDueInProject = soonestTodoDueInProject;
+		this.priorityFlag = priorityFlag;
+		this.completionStatus = completionStatus;
+		this.todoBelongsToProject = todoBelongsToProject;
+		this.creationTimestamp = creationTimestamp;
+		this.lastModifiedTimestamp = lastModifiedTimestamp;
+	}
+})();
