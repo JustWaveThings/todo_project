d054214070329ae3ccfa83851924558d3c883fb7
JustWaveThings <justwavethings@proton.me>
2023-02-13 14:56:20 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-  },
-  devtool: "inline-source-map",
-  devServer: {
-    allowedHosts: [".preview.csb.app", "localhost"],
-    static: "./dist",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "ToDo App",
-      template: "./src/index.html",
-      favicon: "./src/icons8-todo-list-24.png",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
+	mode: 'development',
+	entry: {
+		index: './src/index.js',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		allowedHosts: ['.preview.csb.app', 'localhost'],
+		static: './dist',
+	},
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'ToDo App',
+			template: './src/index.html',
+			favicon: './src/icons8-todo-list-24.png',
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
 };
