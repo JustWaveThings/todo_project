46891ab687e40433cfbca8cf507b4015b7aed4d2
JustWaveThings <justwavethings@proton.me>
2023-02-17 15:48:56 -0500

    Change projectList from const to let

--- src/helpers/project_list.js
+++ src/helpers/project_list.js
@@ -2,7 +2,7 @@ import { todoList } from '../add_todo_popup/todo_dialog_form';
 
 // make an array of projects in the todoList array
 
-const projectList = [];
+let projectList = [];
 
 if (todoList.length > 0) {
 	projectList = todoList.map((todo) => todo.getProject());
